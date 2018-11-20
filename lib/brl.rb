c609600d70a1d1a7b583d22664a4717e50b250de
Mario Maia <101b147ch@gmail.com>
2018-11-20 15:54:59 -0200

    Add ::remove_thousand_dots.

--- lib/brl.rb
+++ lib/brl.rb
@@ -3,5 +3,15 @@ require 'monetize'
 
 module Brl
   class Error < StandardError; end
-  # Your code goes here...
+
+  def self.remove_thousand_dots(str)
+    str = str.to_s
+
+    a = str.split('.')
+    if a.size == 1
+      a[0]
+    else
+      [a[0...-1].join(''), a[-1]].join('.')
+    end
+  end
 end
